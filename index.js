153da041ef977473e4e53e91adb1176a4d7b74bb
JD Tadlock <jt7903@gmail.com>
2024-09-30 13:10:23 -0400

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const num1 = +process.argv[2];
+const num2 = +process.argv[3];
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(num1, num2);
+
+
+
+
+// const data = {
+//   name: 'JD',
+//   info: {
+//     details: {
+//       address: '555 coding dr'
+//     }
+//   }
+// }
+
+// data.info.details.address
