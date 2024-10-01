7dcca79bc8df1eb6c53e8379cc3a143ad478d7aa
JD Tadlock <jt7903@gmail.com>
2024-10-01 13:53:24 -0400

    finished rest/spread basics

--- index.js
+++ index.js
@@ -1,145 +1,29 @@
-const nums = [10, 3, 100, 70, 8, 55, 88];
-const users = [
-  {
-    name: 'Bob',
-    age: 99
-  },
-  {
-    name: 'Jim',
-    age: 25
-  },
-  {
-    name: 'Sarah',
-    age: 40
+const fruits = ['orange', 'apple', 'grape'];
+const fruits2 = ['kiwi', 'banana', 'strawberry'];
+const fruits3 = ['pear', 'peach', 'watermelon'];
+
+const combinedArray = [...fruits, 'dragon fruit', ...fruits3, ...fruits2];
+
+// const concatCombined = fruits.concat(fruits2).concat(another).concat(oneMore);
+
+const data = {
+  name: 'JD',
+  age: 44
+};
+
+
+const combinedData = {
+  ...data,
+  address: '555 coding dr',
+  phone: '777-777-7777'
+};
+
+
+function printNames(...names) {
+  for (const name of names) {
+    console.log(name);
   }
-];
+}
 
-const students = [
-  {
-    name: 'Roman',
-    average: 99
-  },
-  {
-    name: 'Matt',
-    average: 80
-  },
-  {
-    name: 'Izzy',
-    average: 95
-  },
-  {
-    name: 'Kandyce',
-    average: 90
-  }
-]
-
-
-const sum = nums.reduce((output, num) => {
-  return output + num
-}, 0);
-
-// const overallAverage = students.reduce((output, studentObj, index, arr) => {
-//   // If we are on the last student, return the output divided by the arr length
-//   if (index === arr.length - 1) {
-//     return (output + studentObj.average) / arr.length;
-//   }
-
-//   // Otherwise, return output + the studentObj's average
-//   return output + studentObj.average;
-// }, 0);
-
-const studentInfo = students.reduce((output, studentObj, index, arr) => {
-  output.overallAverage += studentObj.average;
-
-  if (index === arr.length - 1) {
-    output.overallAverage = output.overallAverage / arr.length;
-  }
-
-  return output;
-}, {
-  overallAverage: 0,
-  studentCount: students.length
-});
-
-console.log(studentInfo);
-
-// let total = 0;
-
-// for (const studentObj of students) {
-//   total += studentObj.average;
-// }
-
-// const overall = total / students.length;
-
-// console.log(overall);
-
-
-
-const evenArray = nums.map((num) => {
-  if ((num % 2) === 0) {
-    return `${num} is even`
-  } else {
-    return `${num} is odd`
-  }
-});
-
-
-
-
-
-// nums.forEach((num, index) => {
-//   console.log(index);
-// });
-
-
-// for (const num of nums) {
-//   if (num > 10) {
-//     break;
-//   }
-
-//   console.log(num);
-// }
-
-
-
-
-
-
-// Filtering out values/items
-const filteredNums = nums.filter((num) => {
-  if (num > 10) {
-    return true;
-  }
-});
-
-const filteredUsers = users.filter((userObj) => {
-  if (userObj.age > 30) {
-    return true;
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// Algorithm challenge - Write a function that orders the array numbers from lowest to highest
-const sorted = nums.sort((currentNum, nextNum) => {
-  return nextNum - currentNum;
-});
-
-// Create a variable that stores a sorted userArray. In your callback function, return the difference between the current object's age and the next object's age
-const sortedUsers = users.sort((currentObj, nextObj) => {
-  return currentObj.age - nextObj.age;
-});
-
-// Create a variable that stores the sorted users array and sorts the objects by name - hint (all letter characters have a number that you can pull with JS)
-
-
-
+// printNames('Bob', 'Jane', 'Frank', 'Jill', 'Billy');
+printNames('Wilson', 'Harriet');
