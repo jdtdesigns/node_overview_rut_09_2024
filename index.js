5a6da3a66ad7a399498d2a5ac3ce9843ca680a97
JD Tadlock <jt7903@gmail.com>
2024-10-01 10:29:59 -0400

    finished arrow function basics

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const num1 = +process.argv[2];
-const num2 = +process.argv[3];
+const mathTools = {
+  num1: 15,
+  num2: 10,
+  sum: function () {
+    // return the sum of num1 and num2
+    const nested = () => {
+      console.log('nested', this.num1);
+      const moreNested = () => {
+        console.log('moreNested', this.num2);
+      }
 
-function sum(a, b) {
-  console.log(a + b);
+      moreNested();
+    }
+
+    nested();
+
+    return this.num1 + this.num2;
+  },
+  difference: function () {
+    // return the difference of num1 and num2 (ie. num1 - num2)
+    return this.num1 - this.num2;
+  }
+};
+
+const sum = mathTools.sum();
+const diff = mathTools.difference();
+
+console.log('sum', sum);
+console.log('diff', diff);
+
+
+
+
+
+function printName(userName) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const capName = capitalize(userName);
+
+  console.log(capName);
 }
 
-sum(num1, num2);
-
-
-
-
-// const data = {
-//   name: 'JD',
-//   info: {
-//     details: {
-//       address: '555 coding dr'
-//     }
-//   }
-// }
-
-// data.info.details.address
+// printName('bob');
